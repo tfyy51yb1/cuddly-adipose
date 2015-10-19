d9377d22250685cf4d435df65d5e1834249f7483
tfyy51yb1 <noboru_ankare@outlook.com>
2015-10-19 02:54:35 +0200

    Inläggning av filer

---
+++ use_fit_spectral_coefficients.m
@@ -0,0 +1,12 @@
+function [coeff_img, residual] = use_fit_spectral_coefficients %Gör detta till en funktion.
+
+spect_img = Image_gen2(3); %Använd funktionen spect_img för att generera en bild för testningssyften. Måste anpassas för verklig testdata i ett senare skede.
+
+pars = struct('use_affine', false, 'enforce_positive', false); %Jag är inte helt hundra på vilka värden som är att föredra, har valt att inte använda något av dem. Vidare forskning krävs.
+
+refspectra = load_reference_spectra('C:\Users\Oscar\Desktop\tools-dist\reference_spectra', {'adipose', 'hFTAA'}); %Ladda referensspektra. Bör kunna effektivisseras så att detta inte görs separat i flera funktioner.
+%Notera att sökvägen är hårdkodad. Ändras vid senare tillfälle.
+
+[mask, ~, ~] = Use_threshold_signal_image2; %Använd funktionen Use_threshold_signal_images(2) utvärde 'mask' för att maska spect_img. 
+
+[coeff_img, residual] = fit_spectral_coefficients(spect_img, refspectra, mask, pars); %Funktionen fit_spectral_coefficients gör sin magi.
