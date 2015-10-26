bee296508030ca6f601e5dfcff7430bd85a080a2
tfyy51yb1 <noboru_ankare@outlook.com>
2015-10-26 10:36:49 +0100

    Edited my comments.

--- descriptor_calc.m
+++ descriptor_calc.m
@@ -1,6 +1,6 @@
-function descriptor = descriptor_calc(filename, refspectra)%Skapa funktionen. Filename och refspectra tillhandahÂlls av annan funktion.
+function descriptor = descriptor_calc(filename, refspectra)%Skapa funktionen. Filename och refspectra tillhandah√•lls av annan funktion.
 [imgdata, ~] = readASIraw(filename);%Omvandla filename, en fil av typen .raw, till en bildmatris, imgdata.
-[mask, ~, ~] = Use_threshold_signal_image(imgdata);%Anv‰nd utv‰rdet 'mask' frÂn funktionen threshold_signal_image.
-pars = struct('use_affine', false, 'enforce_positive', false);%S‰tt parametrarna 'use_affine' och 'enforce_positive' till false.
+[mask, ~, ~] = Use_threshold_signal_image(imgdata);%Anv√§nd utv√§rdet 'mask' fr√•n funktionen Use_threshold_signal_image.
+pars = struct('use_affine', false, 'enforce_positive', false);%S√§tt parametrarna 'use_affine' och 'enforce_positive' till false.
 [coeff_img, ~] = fit_spectral_coefficients(imgdata, refspectra, mask, pars);%Funktionen fit_spectral_coefficients returnerar spektralkoefficientbilden coeff_img.
-descriptor = calc_foreground_histogram(coeff_img, mask);%Ber‰kna ett histogram av bildens fˆrgrund som ‰r bildens 'descriptor'.
+descriptor = calc_foreground_histogram(coeff_img, mask);%Ber√§kna ett histogram av bildens f√∂rgrund som √§r bildens 'descriptor'.
