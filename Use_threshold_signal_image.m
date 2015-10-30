f290fbbd0ae5b74c860732d0606be6ad5a3f3b38
oscar <noboru_ankare@outlook.com>
2015-10-30 12:01:00 +0100

    Minor Tweaks

--- Use_threshold_signal_image.m
+++ Use_threshold_signal_image.m
@@ -8,14 +8,14 @@ function [mask, threshold_value, ReturnImage] = Use_threshold_signal_image(Image
 
 %%set up parmameter struct
 pars = struct;
-pars.flatten_mode = 'sum'; %% 'sum' or 'max'
+pars.flatten_mode = 'max'; %% 'sum' or 'max'
 pars.normalisefl = false; %%true/false
-pars.method = 'automatic' %% manual/automatic
+pars.method = 'manual' %% manual/automatic
 pars.gammaval = 1 %%gamma value to apply
-pars.threshold_value = 4.404793780967638 %% threshold value
-pars.n_erosion_steps = 1%% number of steps to erode
-pars.n_dilation_steps = 10%% number of steps to dialate
-pars.min_region_size = 1
+pars.threshold_value = 0 %% threshold value
+pars.n_erosion_steps = 0%% number of steps to erode
+pars.n_dilation_steps = 0%% number of steps to dialate
+pars.min_region_size = ceil(size(Image, 1)*size(Image, 2)*0.001)
 
 
 [mask, threshold_value, ReturnImage] = threshold_signal_image(Image, pars); %% Kör threshold-funktionen och spara bara flattended_image
